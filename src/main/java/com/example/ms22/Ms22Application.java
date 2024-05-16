25d57864f0186f0f10bf4c41462f3c8647ce1b9b
RODRIGO <rodrigo.meza@utec.edu.pe>
2024-05-16 11:29:35 -0500

    Push

---
+++ src/main/java/com/example/ms22/Ms22Application.java
@@ -0,0 +1,13 @@
+package com.example.ms22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms22Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ms22/Ms22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
